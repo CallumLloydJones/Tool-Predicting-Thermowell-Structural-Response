e884b0577a21186e67b266e8c36e3294a1207fb2
Callum Lloyd Jones <66999213+CallumLloydJones@users.noreply.github.com>
2023-11-07 23:06:35 +0000

    Delete Full-run-through/elCentre.m

--- Full-run-through/elCentre.m
+++
@@ -1,8 +0,0 @@
-function centre = elCentre(nodes,elements)
-    centre = zeros(length(elements),3);
-    for e = 1:length(elements)
-        a = elements(:,e);
-        npos = [nodes(a(1),:); nodes(a(2),:); nodes(a(3),:); nodes(a(4),:);];
-        centre(e,:) = mean(npos);
-    end
-end
